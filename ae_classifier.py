ca0f86fdedf1c15dcf4fcedfb9343815bee2953e
shostakovich <53115539+tzpyrope@users.noreply.github.com>
2024-12-16 23:01:34 +0100

    Dodanie komentarzy

--- ae_classifier.py
+++ ae_classifier.py
@@ -2,21 +2,31 @@ import tensorflow as tf
 import numpy as np
 
 
+# trenowanie pojedynczego autoenkodera na danych konkretnej klasy
 def train_autoencoder_for_class(autoencoder, class_data, epochs=50, batch_size=256):
     autoencoder.fit(class_data, class_data, epochs=epochs, batch_size=batch_size, shuffle=True, verbose=1)
 
+# klasyfikacja obraz na podstawie błędu rekonstrukcji autoenkoderów
+# reduce_retracing=True: optymalizacja dla tensorflow w celu zmniejszenia liczby ponownych śladów funkcji
+# wcześniej wyskakiwał warning tego dotyczący
 @tf.function(reduce_retracing=True)
 def classify_image(image, autoencoders):
     errors = []
-    image = tf.expand_dims(image, axis=0)  # Dodanie wymiaru batch
+    # dodanie wymiaru batch do obrazu
+    image = tf.expand_dims(image, axis=0)
+
+    # iteracja po autoenkoderach, każdy odpowiada za jedną klasę
     for ae in autoencoders:
-        reconstruction = ae(image, training=False)  # Bez predict
-        error = tf.reduce_mean(tf.square(image - reconstruction))
+        reconstruction = ae(image, training=False) 
+        error = tf.reduce_mean(tf.square(image - reconstruction)) # obliczenie błędu średniokwadratowego
         errors.append(error)
+
+    # zwrócenie indeksu autoenkodera o najmniejszym błędzie (przewidywana klasa)
     return tf.argmin(errors)
 
+# funkcja do trenowania wszystkich autoenkoderów dla poszczególnych klas
 def train_all_autoencoders(X_train, y_train, autoencoders):
-    # Trenowanie autoenkoderów dla każdej klasy
+    # iteracja po indeksach klas oraz odpowiadających im autoenkoderach
     for i, ae in enumerate(autoencoders):
         print(f"Training autoencoder for class {i}")
         class_data = X_train[y_train.flatten() == i]
@@ -24,14 +34,14 @@ def train_all_autoencoders(X_train, y_train, autoencoders):
     
     return autoencoders
 
-# Testowanie klasyfikacji
+# przewidywanie klas dla zbioru testowego
 def make_class_predictions(X_test, autoencoders):
     y_pred = []
     for idx, img in enumerate(X_test): 
         pred_class = classify_image(img, autoencoders)
-        y_pred.append(pred_class.numpy())  # Konwersja tensora na numpy dla dalszego przetwarzania
+        y_pred.append(pred_class.numpy())  # konwersja tensora na numpy dla dalszego przetwarzania
         if idx % 100 == 0:
             print(f"Processed {idx} images")
 
-    y_pred = np.array(y_pred)
+    y_pred = np.array(y_pred) # konwersja listy z przewidywanymi klasami na tablicę numpy
     return y_pred
